d189b9c5099735a1faeb05a2d7fea77345edcf0c
Tim Janssens <timjanssens81@gmail.com>
2019-08-31 18:03:35 +0200

    Add Solution
    Added a solution to the exercise.

--- Sectie 10 Introduction to Javascript/120 Stalker/script.js
+++ Sectie 10 Introduction to Javascript/120 Stalker/script.js
@@ -1,3 +1,8 @@
 var firstName = prompt("What is your first name?");
+
 var lastName = prompt("What is your last name?");
+
 var age = prompt("How old are you?");
+
+console.log("Hello " + firstName + " " + lastName);
+console.log(firstName + " is it correct that you are now " + age + " years old?" );
