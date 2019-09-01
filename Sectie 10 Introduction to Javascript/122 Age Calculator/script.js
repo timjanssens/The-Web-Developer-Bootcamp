16b0bc1b7ff05409946ed536844a945686367fd8
Tim Janssens <timjanssens81@gmail.com>
2019-09-01 09:11:37 +0200

    Add calculation of days
    Finished the exercise.

--- Sectie 10 Introduction to Javascript/122 Age Calculator/script.js
+++ Sectie 10 Introduction to Javascript/122 Age Calculator/script.js
@@ -1,6 +1,26 @@
+//set current date in a var to use afterwards
 var currentDate = new Date();
 
-console.log(currentDate);
+//testing
+//console.log(currentDate);
 
-// dateOfBirth =  prompt("What is your date of birth?");
+var dateOfBirth =  prompt("What is your date of birth?");
+
+//split into an array
+var parts = dateOfBirth.split('/');
+
+//get new date by getting things out of the array and put in date format "year - month - day"
+var myDate = new Date(parts[2], parts[1] - 1, parts[0]); 
+
+//testing
+//console.log(myDate.toDateString());
+
+var ageInMilliseconds = currentDate - myDate;
+
+//The number of milliseconds in one day
+var oneDay = 1000 * 60 * 60 * 24;
+
+var ageInDays = ageInMilliseconds / oneDay;
+
+console.log("You are " +  Math.floor(ageInDays) + " days old.");
 
