080ecaed1835a146724d06430bcfffb59ffabca3
Tim Janssens <timjanssens81@gmail.com>
2019-09-02 20:58:54 +0200

    Add logic new task, list, quit

--- Sectie 13 Javascript Basics Arrays/158 Todo List Code Along Part 1/script.js
+++ Sectie 13 Javascript Basics Arrays/158 Todo List Code Along Part 1/script.js
@@ -1,1 +1,20 @@
-alert("test")
+var answer = "test";
+var task = "";
+var toDos = [];
+
+while (answer !== "quit") {
+
+    answer = prompt("What would you like to do?");
+
+    if (answer === "new") {
+        //ask which task
+        task = prompt("What task do you want to add?");
+        //add a todo
+        toDos.push(task);
+    }
+    else if (answer === "list") {
+        //view all to do
+        console.log(toDos);
+    }
+
+}
