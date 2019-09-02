b6ac30872b9af9a59934ffcd8e24a0de11c957c4
Tim Janssens <timjanssens81@gmail.com>
2019-09-02 10:09:52 +0200

    Add solution check even/odd

---
+++ Sectie 12 Javascript Basics Functions/148 Functions Problem Set/script.js
@@ -0,0 +1,17 @@
+
+//for testing
+// function sayHi(str) {
+//     return str;
+// }
+// // sayHi("hello")
+
+
+function isEven(numE) {
+    if (numE % 2 ===0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
