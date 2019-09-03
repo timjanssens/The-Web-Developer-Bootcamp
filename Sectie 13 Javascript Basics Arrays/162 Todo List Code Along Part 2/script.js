c0de22857ac3a12c50cf827409f8b78ed50958fa
Tim Janssens <timjanssens81@gmail.com>
2019-09-03 14:20:14 +0200

    Finish exercise

--- Sectie 13 Javascript Basics Arrays/162 Todo List Code Along Part 2/script.js
+++ Sectie 13 Javascript Basics Arrays/162 Todo List Code Along Part 2/script.js
@@ -1,9 +1,11 @@
 var answer = "test";
 var task = "";
 var toDos = [];
+var toDelete =""
 
 while (answer !== "quit") {
 
+    // 
     answer = prompt("What would you like to do?");
 
     if (answer === "new") {
@@ -12,9 +14,30 @@ while (answer !== "quit") {
         //add a todo
         toDos.push(task);
     }
+    
     else if (answer === "list") {
         //view all to do
-        console.log(toDos);
+        console.log("***********");
+        //make function to print each item but don't execute
+        function logArrayElements(element, index){
+            console.log(index + ":   " +element);
+        }
+
+        //go through the array toDos and print console.log
+        toDos.forEach(logArrayElements);
+        console.log("***********");
     }
 
+    else if (answer === "delete") {
+
+        toDelete = prompt("Enter index of todo to delete");
+
+        //delete te element that is on the index of toDelete, 1 item
+        toDos.splice(toDelete,1);
+
+    }   
+
+
 }
+
+console.log("You ended the program");
