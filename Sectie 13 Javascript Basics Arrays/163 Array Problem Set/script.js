075c8ae7a880b27e8495546ba255d2c7714de96b
Tim Janssens <timjanssens81@gmail.com>
2019-09-03 19:48:35 +0200

    Solve second exercise

--- Sectie 13 Javascript Basics Arrays/163 Array Problem Set/script.js
+++ Sectie 13 Javascript Basics Arrays/163 Array Problem Set/script.js
@@ -1,7 +1,32 @@
-
-function printReverse(items) {
-    for (i = (items.length-1) ; i >= 0 ; i--) {
-        console.log(items[i]);
+//reverse the array
+function printReverse(array) {
+    for (i = (array.length-1) ; i >= 0 ; i--) {
+        console.log(array[i]);
      }
     }
 
+//check if the array is uniform (all the same)
+
+
+function isUniform(array) {
+    
+    for (i = 0 ; i < (array.length-1) ; i++) {
+        if (array[i] !== array[i+1]) {
+          return  false;
+        } 
+    }
+    return true;
+}
+
+//second solution
+
+// function isUniform(array) {
+//     var first = array[0];
+//     for(var i = 1 ; i < array.length ; i++){
+//         if (array[i] !== first){
+//             //return false and end the function
+//             return false;
+//         }
+//     }
+//     return true;
+// }
