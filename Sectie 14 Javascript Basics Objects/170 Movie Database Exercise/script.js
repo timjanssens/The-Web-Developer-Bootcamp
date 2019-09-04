3aceb99bbb11adcc3aabd7dceebdf8478b8120c4
Tim Janssens <timjanssens81@gmail.com>
2019-09-04 14:52:59 +0200

    Add text for seen or not seen
    Created extra function and called it in the function

--- Sectie 14 Javascript Basics Objects/170 Movie Database Exercise/script.js
+++ Sectie 14 Javascript Basics Objects/170 Movie Database Exercise/script.js
@@ -3,3 +3,18 @@ var movies = [
     {name: "The good wife", rating:4, seen: true}, 
     {name: "Suits", rating:5, seen: true} 
 ];
+
+function checkSeen(movies) {
+    if (movies.seen === true){
+        return "You have watched";
+    }
+    else {
+        return "You have not seen";
+    }
+}
+
+
+
+movies.forEach (function(movie){
+    console.log(checkSeen(movie) + " \"" + movie.name + "\"" + " - " + movie.rating + " stars");
+});
