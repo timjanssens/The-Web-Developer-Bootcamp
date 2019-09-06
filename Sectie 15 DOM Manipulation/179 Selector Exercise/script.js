9d6a89deeb1305a1771995a1fc79601aa67a9f8b
Tim Janssens <timjanssens81@gmail.com>
2019-09-06 10:44:06 +0200

    Add solution select in 4 different ways

---
+++ Sectie 15 DOM Manipulation/179 Selector Exercise/script.js
@@ -0,0 +1,7 @@
+var first = document.getElementById("first");
+
+var second = document.querySelector(".special");
+
+var third = document.querySelectorAll("#first");
+
+var fourth = document.querySelector("p");
