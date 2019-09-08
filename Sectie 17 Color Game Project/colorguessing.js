6fa64b3ceb8f41ca7e4a2b1b792a00e8bf58ae54
Tim Janssens <timjanssens81@gmail.com>
2019-09-08 18:32:44 +0200

    Add collor changer

--- Sectie 17 Color Game Project/colorguessing.js
+++ Sectie 17 Color Game Project/colorguessing.js
@@ -1,1 +1,24 @@
-alert("connected");
+var redRGB = oneRGB();
+var greenRGB = oneRGB();
+var blueRGD = oneRGB();
+
+var correctRGB = (redRGB+", " + greenRGB+", " + blueRGD);
+
+var newColorLi = document.getElementById("newColorLi");
+var guessingRGB = document.getElementById("colorTextRGB");
+
+
+newColorLi.addEventListener("click", function(){
+    
+    guessingRGB.innerHTML = "RGB " + correctRGB;
+}
+)
+
+//generate a random between 0 an 255
+function oneRGB() {
+    return Math.floor(Math.random()*255);
+}
+
+
+
+console.log(correctRGB);
