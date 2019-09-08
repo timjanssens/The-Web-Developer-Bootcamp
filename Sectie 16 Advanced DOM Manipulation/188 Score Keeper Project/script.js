fd060d538f60ef043926211f7725bfdb4947e8ba
Tim Janssens <timjanssens81@gmail.com>
2019-09-08 09:10:39 +0200

    Set up files and Add solution

--- Sectie 16 Advanced DOM Manipulation/188 Score Keeper Project/script.js
+++ Sectie 16 Advanced DOM Manipulation/188 Score Keeper Project/script.js
@@ -18,7 +18,7 @@ document.getElementById("pointPlayerOne").onclick = function() {
 //add point to player if the max score isn't reached
 //when the max score is reached turn green and make it
 //impossible to add score to anyone
-document.getElementById("pointPlayerTwo").onclick = function() {
+document.getElementById("pointPlayerTwo").onchange = function() {
     if (scorePlayerTwo < playingTo-1 && scorePlayerOne < playingTo) {
     scorePlayerTwo++;
     document.getElementById("scorePlayerTwo").innerHTML = scorePlayerTwo;
@@ -29,7 +29,8 @@ document.getElementById("pointPlayerTwo").onclick = function() {
     }}
 
 //reset both scores to zero and also the max games to standard
-document.getElementById("resetButton").onclick = function(){
+//Tried it with an addEventListener
+document.getElementById("resetButton").addEventListener("click", function(){
     scorePlayerOne = 0;
     scorePlayerTwo = 0;
     playingTo = 5;
@@ -39,7 +40,7 @@ document.getElementById("resetButton").onclick = function(){
     document.getElementById("scorePlayerTwo").style.color = "black";
     document.getElementById("playingTo").innerHTML = playingTo;
     document.getElementById("playingToAdjustor").value = "";
-}
+});
 
 //when the game is not yet started, make it possible to change the max score
 document.getElementById("playingToAdjustor").onclick = function () {
